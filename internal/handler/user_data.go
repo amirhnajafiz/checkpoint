846c8c6fad469951a8308908c07db70ee387cb51
amirhnajafiz <najafizadeh21@gmail.com>
2021-12-24 19:23:46 +0330

    get data handler added

---
+++ internal/handler/user_data.go
@@ -0,0 +1,14 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func GetData(w http.ResponseWriter, r *http.Request) {
+	username := r.FormValue("username")
+
+	// TODO: get user data from database
+
+	_, _ = fmt.Fprint(w, username)
+}
--- internal/server/main.go
+++ internal/server/main.go
@@ -2,12 +2,17 @@ package server
 
 import (
 	"cmd/internal/handler"
+	"cmd/middleware/auth"
 	"log"
 	"net/http"
 )
 
 func HandleRequests() {
+	// Auth routes
 	http.HandleFunc("/api/login", handler.Login)
 	http.HandleFunc("/api/register", handler.Register)
+	// Web routes
+	http.Handle("/api/user", auth.IsAuthorized(handler.GetData))
+
 	log.Fatal(http.ListenAndServe(":5001", nil))
 }
