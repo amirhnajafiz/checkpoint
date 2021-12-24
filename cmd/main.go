adeda5df4a00b799854126ff70ba693c9ab0244f
amirhnajafiz <najafizadeh21@gmail.com>
2021-12-24 19:19:55 +0330

    server routes added

--- cmd/main.go
+++ cmd/main.go
@@ -1,29 +1,11 @@
 package main
 
 import (
-	"cmd/internal/jsonwebtoken"
-	"cmd/middleware/auth"
+	"cmd/internal/server"
 	"fmt"
-	"log"
-	"net/http"
 )
 
-func homePage(w http.ResponseWriter, _ *http.Request) {
-	validation, err := jsonwebtoken.GenerateToken()
-
-	if err != nil {
-		_, _ = fmt.Fprintf(w, err.Error())
-	}
-
-	_, _ = fmt.Fprintf(w, validation)
-}
-
-func handleRequests() {
-	http.Handle("/", auth.IsAuthorized(homePage))
-	log.Fatal(http.ListenAndServe(":5001", nil))
-}
-
 func main() {
-	fmt.Println("Lets go ...")
-	handleRequests()
+	fmt.Println("API server started ...")
+	server.HandleRequests()
 }
--- internal/server/main.go
+++ internal/server/main.go
@@ -1,1 +1,13 @@
 package server
+
+import (
+	"cmd/internal/handler"
+	"log"
+	"net/http"
+)
+
+func HandleRequests() {
+	http.HandleFunc("/api/login", handler.Login)
+	http.HandleFunc("/api/register", handler.Register)
+	log.Fatal(http.ListenAndServe(":5001", nil))
+}
