e84d81636dacdc689bd5a9b47b9a6a7e33d9d9dd
amirhnajafiz <najafizadeh21@gmail.com>
2021-12-24 19:17:30 +0330

    register handler added

--- internal/handler/sign_in.go
+++ internal/handler/sign_in.go
@@ -1,1 +1,15 @@
 package handler
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Register(w http.ResponseWriter, r *http.Request) {
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+
+	// TODO: Save to database
+
+	_, _ = fmt.Fprint(w, username+":"+password)
+}
