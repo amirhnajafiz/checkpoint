eda268bd932ecf7f5f2878c4843e571ec0fe963c
amirhnajafiz <najafizadeh21@gmail.com>
2021-12-24 19:34:27 +0330

    airbrake imported and configured

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"cmd/internal/server"
+	"errors"
 	"fmt"
+	"github.com/airbrake/gobrake/v5"
 )
 
+var airbrake = gobrake.NewNotifierWithOptions(&gobrake.NotifierOptions{
+	ProjectId:   384477,
+	ProjectKey:  "1914b401317b91a2192d0f899c8ad943",
+	Environment: "production",
+})
+
 func main() {
+	defer airbrake.Close()
+	defer airbrake.NotifyOnPanic()
 	fmt.Println("API server started ...")
+	airbrake.Notify(errors.New("test from Airbrake"), nil)
 	server.HandleRequests()
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module cmd
 
 go 1.17
 
-require github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+require (
+	github.com/airbrake/gobrake/v5 v5.2.0 // indirect
+	github.com/caio/go-tdigest v3.1.0+incompatible // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/jonboulle/clockwork v0.2.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+)
