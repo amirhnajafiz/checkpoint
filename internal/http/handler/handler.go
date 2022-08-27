a09defe5da6525572783ec4211ed0dd685ac768d
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-27 10:24:21 +0430

    fix: handler methods

--- internal/http/handler/handler.go
+++ internal/http/handler/handler.go
@@ -3,54 +3,36 @@ package handler
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/amirhnajafiz/checkpoint/internal/jwt"
 	"net/http"
 	"strings"
+
+	"github.com/amirhnajafiz/checkpoint/internal/jwt"
 )
 
 func Login(w http.ResponseWriter, r *http.Request) {
 	username := r.FormValue("username")
 	password := r.FormValue("password")
 
-	// TODO: check the user model is database
-
-	_ = json.NewEncoder(w).Encode(HandleLogin(username, password))
-}
-
-func HandleLogin(username string, password string) map[string]string {
 	response := make(map[string]string)
 	token, err := jwt.GenerateToken(username + password)
 
-	response["Token"] = token
+	response["token"] = token
 
 	if err != nil {
-		response["Token"] = "nil"
+		response["token"] = "nil"
 	}
 
-	return response
+	_ = json.NewEncoder(w).Encode(response)
 }
 
 func Register(w http.ResponseWriter, r *http.Request) {
 	username := r.FormValue("username")
 	password := r.FormValue("password")
 
-	// TODO: Save to database
-
-	_, _ = fmt.Fprint(w, HandleRegister(username, password))
-}
-
-func HandleRegister(username string, password string) string {
-	return username + ":" + password
+	_, _ = fmt.Fprint(w, username+":"+password)
 }
 
 func GetData(w http.ResponseWriter, r *http.Request) {
 	username := r.FormValue("username")
-
-	// TODO: get user data from database
-
-	_, _ = fmt.Fprint(w, HandleGetData(username))
-}
-
-func HandleGetData(username string) string {
-	return strings.ToUpper(username)
+	_, _ = fmt.Fprint(w, strings.ToUpper(username))
 }
