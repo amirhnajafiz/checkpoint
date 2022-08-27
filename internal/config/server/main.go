a15fc8f3bb7f395627e88f5f503e77cc82a1f781
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-27 08:01:35 +0430

    fix: bugs in middleware refactor

--- internal/config/server/main.go
+++ internal/config/server/main.go
@@ -1,10 +1,10 @@
 package server
 
 import (
-	"github.com/amirhnajafiz/checkpoint/internal/handler/login"
-	"github.com/amirhnajafiz/checkpoint/internal/handler/sign_in"
-	"github.com/amirhnajafiz/checkpoint/internal/handler/user_data"
-	"github.com/amirhnajafiz/checkpoint/internal/middleware/auth"
+	"github.com/amirhnajafiz/checkpoint/internal/http/handler/login"
+	"github.com/amirhnajafiz/checkpoint/internal/http/handler/sign_in"
+	"github.com/amirhnajafiz/checkpoint/internal/http/handler/user_data"
+	"github.com/amirhnajafiz/checkpoint/internal/http/middleware"
 	"log"
 	"net/http"
 )
@@ -14,7 +14,7 @@ func HandleRequests() {
 	http.HandleFunc("/api/login", login.Login)
 	http.HandleFunc("/api/register", sign_in.Register)
 	// Web routes
-	http.Handle("/api/user", auth.IsAuthorized(user_data.GetData))
+	http.Handle("/api/user", middleware.Auth(user_data.GetData))
 
 	log.Fatal(http.ListenAndServe(":5001", nil))
 }
